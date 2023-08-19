b544a7b8cc59aa4c84801676ad750bba992ada58
basmaEldesouky <basma.eldesouky@gmail.com>
2023-08-19 07:09:36 +0300

    Setting up presentation layer by implementing activities, and viewmodels

--- domain/src/main/java/com/example/domain/useCase/GetLatestRatesUseCase.kt
+++ domain/src/main/java/com/example/domain/useCase/GetLatestRatesUseCase.kt
@@ -4,7 +4,7 @@ import com.example.domain.entity.LatestRate
 import com.example.domain.repo.CurrencyRepositoryContract
 import kotlinx.coroutines.flow.Flow
 
-class GetLatestRates(private val repository: CurrencyRepositoryContract) {
+class GetLatestRatesUseCase(private val repository: CurrencyRepositoryContract) {
     suspend fun invoke(base: String, symbols: List<String>): Flow<LatestRate> {
         return repository.getLatestRates(base, symbols)
     }
